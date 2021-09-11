f79a74cf6e867ef20bcc449df839e36cd116f248
Ghidorah <jljohnson1454@gmail.com>
2021-09-11 11:06:04 -0500

    fixed text area

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,63 +3,90 @@
 // var taskLiEl = $(".list-group").append('<li class="list-group-item"></li>');
 // var saveButtonEl = $('.container-fluid').append('<button id="save" class="saveBtn">Save</button>');
 
-var timeofDay = ["9AM","10AM","11AM","12PM", "1PM", "2PM", "3PM", "4PM", "5PM"] 
+var timeofDay = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+
+// var timeofDay = [
+// {
+//     hour: "9",
+//     meridiem:"am"},
+// {
+//     hour:"10",
+//     meridiem:"am"},
+// {
+//     hour:"11",
+//     meridiem:"am"
+// },
+// {
+//     hour:"12",
+//     meridiem:"pm"
+// },
+// {
+//     hour:"1",
+//     meridiem:"pm"},
+// {
+//     hour:"2",
+//     meridiem:"pm"},
+// {
+//     hour:"3",
+//     meridiem:"pm"
+// },
+// {
+//     hour:"4",
+//     meridiem:"pm"
+// },
+// {
+//     hour:"5",
+//     meridiem:"pm"
+// }]
 
 // Current time
-var currentDay = function() {
+
     var currentDayEl = $('#currentDay.lead');
     var today = moment().format('LLL');
 
     $(currentDayEl).html(today);
-    };
 
 // Time Blocks
     for (var i = 1; i < 9; i++) {
 
         $(".container-fluid").append(
-            `<div class ="row">
-                <div id=time class="col-2 m-0 p-0 border border-dark border-right-0">
+            `<div class ="row time-block">
+                <div id=time class="col-1 m-0 p-0 border border-dark border-right-0">
                     ${timeofDay[i]}
                 </div>
-                <div class="col-8 p-0 border border-dark">
-                    <ul class="list-group">
-                        <li class="list-group-item p-0">Blah</li>
-                    </ul>           
+                <div id=textArea class="col-10 p-0 border border-dark">         
                 </div>
-                <div class="saveBtn col-2 p-0 border border-dark">
-                <span class="oi oi-hard-drive"></span>
-                </div>
-            </div>`)      
-}
+                <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>
+            </div>`)    
+        }
 
 
 //Edit Text in Time Blocks
 
-$(".list-group").on("click", "li", function() {
-    var text = $(this)
-      .text()
-      .trim();
+$("div").on("click", function(e) {
+    if(e.target.getAttribute('id') === 'textArea'){
       var textInput = $("<textarea>")
-      .addClass("form-control")
-      .val(text);
-      $(this).replaceWith(textInput);
+      .addClass("col-md-10 description")
+      $(e.target).replaceWith(textInput);
       textInput.trigger("focus");
+    }
+
       });
 
 $(".list-group").on("blur", "textarea", function() {
-    var text=$(this)
+    text=$(this)
         .val()
         .trim();
-
-    var status = $(this)
-        .closest(".list-group")
-        .attr("id")
-        .replace("list-", "");
-    
-    var index = $(this)
-        .closest(".list-group-item")
-        .index();
 })
+
+// Past,Present, Future color coding
+if(timeofDay[i] < moment().format("HH")) {
+    $('#time').attr(".past");
+} else if(timeofDay[i] > moment().format("HH")){
+    $('#time').attr(".future");
+} else if(timeofDay[i] === moment().format("HH")) {
+    $('#time').attr(".future");
+}
 
 
 // Load tasks from local storage
@@ -70,27 +97,21 @@ $(".list-group").on("blur", "textarea", function() {
 
 // Save button in local storage
 $(".saveBtn").on("click", function(){
-    
+    saveTasks();
+
+    var description = $(this).siblings[".description"].val();
 })
 
-// var saveTasks = function() {
-    //localStorage.setItem("timeofDay")
-//}
+    var saveTasks = function() {
+    localStorage.setItem("timeofDay",)
+}
 
 
-
-
-
-    // Create 8 time blocks for standard business hours 9am-5pm
-
-    
     // Navigate the DOM (this) 
 
 
 
     // Each time block is color-coded to indicate whether it is in the past, present, or future
-
-    //When I click into a time block, I can enter text(event)
 
     //When I click the save button, the text for that event is saved in local storage
 
