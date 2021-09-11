134be4a44fe57dcb7b81824c719f3bc3f7467765
Ghidorah <jljohnson1454@gmail.com>
2021-09-11 15:27:03 -0500

    fixed width

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,41 +3,50 @@
 // var taskLiEl = $(".list-group").append('<li class="list-group-item"></li>');
 // var saveButtonEl = $('.container-fluid').append('<button id="save" class="saveBtn">Save</button>');
 
-var timeofDay = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+// var timeofDay = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
 
-// var timeofDay = [
-// {
-//     hour: "9",
-//     meridiem:"am"},
-// {
-//     hour:"10",
-//     meridiem:"am"},
-// {
-//     hour:"11",
-//     meridiem:"am"
-// },
-// {
-//     hour:"12",
-//     meridiem:"pm"
-// },
-// {
-//     hour:"1",
-//     meridiem:"pm"},
-// {
-//     hour:"2",
-//     meridiem:"pm"},
-// {
-//     hour:"3",
-//     meridiem:"pm"
-// },
-// {
-//     hour:"4",
-//     meridiem:"pm"
-// },
-// {
-//     hour:"5",
-//     meridiem:"pm"
-// }]
+var timeofDay = [
+{
+    military: "9",
+    hour: "9",
+    meridiem:"am"},
+{
+    military: "10",
+    hour:"10",
+    meridiem:"am"},
+{
+    military: "11",
+    hour:"11",
+    meridiem:"am"
+},
+{
+    military: "12",
+    hour:"12",
+    meridiem:"pm"
+},
+{
+    military: "13",
+    hour:"1",
+    meridiem:"pm"},
+{
+    military: "14",
+    hour:"2",
+    meridiem:"pm"},
+{
+    military: "15",
+    hour:"3",
+    meridiem:"pm"
+},
+{
+    military: "16",
+    hour:"4",
+    meridiem:"pm"
+},
+{
+    military: "17",
+    hour:"5",
+    meridiem:"pm"
+}]
 
 // Current time
 
@@ -47,15 +56,14 @@ var timeofDay = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm
     $(currentDayEl).html(today);
 
 // Time Blocks
-    for (var i = 1; i < 9; i++) {
+    for (var i = 0; i < 9; i++) {
 
         $(".container-fluid").append(
-            `<div class ="row time-block">
-                <div id=time class="col-1 m-0 p-0 border border-dark border-right-0">
-                    ${timeofDay[i]}
+            `<div class ="row">
+                <div id=time class="hour col-1 m-0 p-0 border border-dark border-right-0 rounded-left">
+                    ${timeofDay[i].hour}
                 </div>
-                <div id=textArea class="col-10 p-0 border border-dark">         
-                </div>
+                <div id=textArea class="col-10 p-0 border border-dark"></div>
                 <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>
             </div>`)    
         }
@@ -70,22 +78,16 @@ $("div").on("click", function(e) {
       $(e.target).replaceWith(textInput);
       textInput.trigger("focus");
     }
-
       });
 
-$(".list-group").on("blur", "textarea", function() {
-    text=$(this)
-        .val()
-        .trim();
-})
 
-// Past,Present, Future color coding
-if(timeofDay[i] < moment().format("HH")) {
-    $('#time').attr(".past");
-} else if(timeofDay[i] > moment().format("HH")){
-    $('#time').attr(".future");
-} else if(timeofDay[i] === moment().format("HH")) {
-    $('#time').attr(".future");
+// Past, Present, Future color coding
+if(timeofDay < moment().format("HH")) {
+    $('#textArea').addClass("past");
+} else if(timeofDay > moment().format("HH")){
+    $('#textArea').addClass("future");
+} else if(timeofDay === moment().format("HH")) {
+    $('#textArea').addClass("future");
 }
 
 
@@ -97,23 +99,28 @@ if(timeofDay[i] < moment().format("HH")) {
 
 // Save button in local storage
 $(".saveBtn").on("click", function(){
-    saveTasks();
 
-    var description = $(this).siblings[".description"].val();
+    var textValue = $(this).siblings("#textArea").val();
+    var timeValue = $(this).siblings("#textArea").attr("hour");
+
+    localStorage.setItem(timeValue, textValue)
+
 })
+
+localStorage.getItem(11)
+
 
     var saveTasks = function() {
     localStorage.setItem("timeofDay",)
 }
 
+function loadStorage() {
+    var array = [9,10,11,12,1,2,3,4,5];
+    array.forEach(function(e){
+        $("textarea[hour='" + e + "']").val(localStorage.getItem(e));
+    })
+}
 
-    // Navigate the DOM (this) 
 
 
-
-    // Each time block is color-coded to indicate whether it is in the past, present, or future
-
-    //When I click the save button, the text for that event is saved in local storage
-
-    //When I refresh the page, the saved events still exist
-
+loadStorage();
